3a3d9f59dd14158ced268bc551d7d25d85ee83a3
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-06-26 20:50:33 -0400

    Stretch complete

--- names/stretch.py
+++ names/stretch.py
@@ -1,36 +1,40 @@
 import time
-from binary_search_tree import BSTNode
-
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
+all_names = names_1 + names_2
+
 
 duplicates = []  # Return the list of duplicates in this data structure
-binary = BSTNode('test')
+
+
+def checkIfDuplicates_1(listOfElems):
+    ''' Check if given list contains any duplicates '''
+    if len(listOfElems) == len(set(listOfElems)):
+        return False
+    else:
+        return True
+
+
+result = checkIfDuplicates_1(all_names)
+if result:
+    print('Yes, list contains duplicates')
+else:
+    print('No duplicates found in list')
+
+
 # Replace the nested for loops below with your improvements
-# time complexity O(n^2)
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
-for i in names_1:
-    binary.insert(i)
-for i in names_2:
-    if binary.contains(i):
-        duplicates.append(i)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
-
-# ---------- Stretch Goal -----------
-# Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
-# structures, but you may not import any additional libraries that you did not write yourself.
